c461079600e6434ee1ee09309c81e2c346fdee0d
grybrys <113046543+GabrielaSolak@users.noreply.github.com>
2025-04-15 15:33:21 +0200

    15.04.2025

--- projekt_app/projekt_app/MainWindow.xaml.cs
+++ projekt_app/projekt_app/MainWindow.xaml.cs
@@ -25,6 +25,64 @@ namespace projekt_app
         {
             dodajUzytkownika dodajUzytkownika = new();
             dodajUzytkownika.ShowDialog();
+
+            Osoba uczen = new();
+            uczen.m_strPesel = dodajUzytkownika.pesel.Text;
+            uczen.m_strImie = dodajUzytkownika.imie.Text;
+            uczen.m_strDrugieImie = dodajUzytkownika.drugie_imie.Text;
+            uczen.m_strNazwisko = dodajUzytkownika.nazwisko.Text;
+            uczen.m_strDataUr = dodajUzytkownika.data_ur.Text;
+            uczen.m_strTelefon = dodajUzytkownika.telefon.Text;
+            uczen.m_strAdres = dodajUzytkownika.adres.Text;
+            uczen.m_strMiejscowosc = dodajUzytkownika.miejscowosc.Text;
+            uczen.m_strKodPocztowy = dodajUzytkownika.kod_pocztowy.Text;
+
+            listaUczniow.Items.Add(uczen);
+
+        }
+
+        class Osoba
+        {
+            public string? m_strPesel { get; set; }
+            public string? m_strImie { get; set; }
+            public string? m_strDrugieImie { get; set; }
+            public string? m_strNazwisko { get; set; }
+            public string? m_strDataUr { get; set; }
+            public string? m_strTelefon { get; set; }
+            public string? m_strAdres { get; set; }
+            public string? m_strMiejscowosc { get; set; }
+            public string? m_strKodPocztowy { get; set; }
+            public Osoba()
+            {
+                m_strPesel = "00000000000";
+                m_strImie = "";
+                m_strDrugieImie = "";
+                m_strNazwisko = "";
+                m_strDataUr = "";
+                m_strTelefon = "";
+                m_strAdres = "";
+                m_strMiejscowosc = "";
+                m_strKodPocztowy = "";
+
+            }
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void NewRecord_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void RemoveSel_Click(object sender, RoutedEventArgs e)
+        {
+            while (listaUczniow.SelectedItems.Count > 0)
+            {
+                listaUczniow.Items.Remove(listaUczniow.SelectedItems[0]);
+            }
         }
     }
 }
--- projekt_app/projekt_app/dodajUzytkownika.xaml.cs
+++ projekt_app/projekt_app/dodajUzytkownika.xaml.cs
@@ -23,5 +23,12 @@ namespace projekt_app
         {
             InitializeComponent();
         }
+
+        
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
     }
 }
